218c05a4f841ddc0df0a53e2e6154a35077c6271
jazmindestefano <jazminisabeldestefano@gmail.com>
2022-11-22 01:02:00 -0300

    nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -37,11 +37,39 @@ const NavBar = () => {
         pr={36}
         gap={10}
       >
-        <RouteLink href="/home"><Link color={location === "/home" ? colors.buttonGreen : "white"}>Home</Link></RouteLink>
-        <RouteLink href="/materias"><Link color={location === "/materias" ? colors.buttonGreen : "white"}>Materias</Link></RouteLink>
-        <RouteLink href="/notas"><Link color={location === "/notas" ? colors.buttonGreen : "white"}>Notas</Link></RouteLink>
-        <RouteLink href="/mensajeria"><Link color={location === "/mensajeria" ? colors.buttonGreen : "white"}>Mensajería</Link></RouteLink>
-        <RouteLink href="/calendario-academico"><Link color={location === "/calendario-academico" ? colors.buttonGreen : "white"}>Calendario académico</Link></RouteLink>
+        <RouteLink href="/home">
+          <Link color={location === "/home" ? colors.buttonGreen : "white"}>
+            Home
+          </Link>
+        </RouteLink>
+        <RouteLink href="/materias">
+          <Link color={location === "/materias" ? colors.buttonGreen : "white"}>
+            Materias
+          </Link>
+        </RouteLink>
+        <RouteLink href="/notas">
+          <Link color={location === "/notas" ? colors.buttonGreen : "white"}>
+            Notas
+          </Link>
+        </RouteLink>
+        <RouteLink href="/mensajeria">
+          <Link
+            color={location === "/mensajeria" ? colors.buttonGreen : "white"}
+          >
+            Mensajería
+          </Link>
+        </RouteLink>
+        <RouteLink href="/calendario-academico">
+          <Link
+            color={
+              location === "/calendario-academico"
+                ? colors.buttonGreen
+                : "white"
+            }
+          >
+            Calendario académico
+          </Link>
+        </RouteLink>
       </Stack>
     </Stack>
   );
